6f0a422f5e87161ef0a12b45da016494ad252549
SEEF <seifeddine.gherir@etudiant-fst.utm.tn>
2023-08-08 15:42:36 +0100

    Added modal component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,15 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import Layout from '../components/Layout'
+import Modal from '@/components/Modal'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-  <Layout> 
-    <Component {...pageProps} />
-  </Layout>)
+    <>
+      <Modal isOpen title="testModal" actionLabel="actionLabel" />
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
+  )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,7 @@ import Header from "@/components/Header";
 export default function Home() {
   return (
     <>
-      <Header showBackArrow label="Home"/>
+      <Header label="Home" />
     </>
   )
 }
