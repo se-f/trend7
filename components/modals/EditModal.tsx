48ce59c59023b7fc303eccc9451ad80f79a17e67
SEEF <seifeddine.gherir@etudiant-fst.utm.tn>
2023-08-11 16:42:53 +0100

    Added profile and cover image upload

--- components/modals/EditModal.tsx
+++ components/modals/EditModal.tsx
@@ -6,6 +6,7 @@ import { useCallback, useEffect, useState } from "react";
 import toast from "react-hot-toast";
 import Modal from "../Modal";
 import Input from "../Input";
+import ImageUpload from "../ImageUpload";
 
 const EditModal = () => {
 
@@ -55,6 +56,16 @@ const EditModal = () => {
 
     const bodyContent = (
         <div className="flex flex-col gap-4">
+            <ImageUpload
+                value={profileImage}
+                disabled={isLoading}
+                onChange={(image) => setProfileImage(image)}
+                label="Upload profile image" />
+            <ImageUpload
+                value={coverImage}
+                disabled={isLoading}
+                onChange={(image) => setCoverImage(image)}
+                label="Upload cover image" />
             <Input
                 placeholder="Name"
                 onChange={(e) => setName(e.target.value)}
