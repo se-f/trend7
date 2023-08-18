ceac3e77ec215d6aa8b23eb303d7f064e68a908f
SEEF <seifeddine.gherir@etudiant-fst.utm.tn>
2023-08-18 16:25:52 +0100

    improved performance by altering avatar

--- components/Avatar.tsx
+++ components/Avatar.tsx
@@ -38,6 +38,8 @@ const Avatar: React.FC<AvatarProps> = ({ userId, isLarge, hasBorder }) => {
         >
             <Image
                 fill
+                sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+
                 style={{
                     objectFit: 'cover',
                     borderRadius: '100%'
