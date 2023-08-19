fa86a3cb06467d059f6215fd733041160f697c1a
SEEF <seifeddine.gherir@etudiant-fst.utm.tn>
2023-08-19 17:04:29 +0100

    Added notification for following

--- pages/api/follow.ts
+++ pages/api/follow.ts
@@ -1,52 +1,74 @@
 import serverAuth from "@/libs/serverAuth";
 import { NextApiRequest, NextApiResponse } from "next";
-import prisma from "@/libs/PrismaDB"
+import prisma from "@/libs/PrismaDB";
 
-export default async function handler(req: NextApiRequest,
-    res: NextApiResponse) {
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method !== "POST" && req.method !== "DELETE") {
+    return res.status(405).end();
+  }
 
-    if (req.method !== "POST" && req.method !== "DELETE") {
-        return res.status(405).end()
+  try {
+    const userId = req.method === "POST" ? req.body.userId : req.query.userId;
+
+    const { currentUser } = await serverAuth(req, res);
+
+    if (!userId || typeof userId !== "string") {
+      throw new Error("Invalid ID follow.ts");
+    }
+
+    const user = await prisma.user.findUnique({
+      where: {
+        id: userId,
+      },
+    });
+
+    if (!user) {
+      throw new Error("Invalid ID follow.ts (2)");
+    }
+
+    let updatedFollowingIds = [...(user.followingIds || [])];
+
+    if (req.method == "POST") {
+      updatedFollowingIds.push(userId);
     }
 
     try {
-        const userId = req.method === 'POST' ? req.body.userId : req.query.userId;
+      await prisma.notification.create({
+        data: {
+          body: "Someone followed you! :D ",
+          userId,
+        },
+      });
 
-        const { currentUser } = await serverAuth(req, res)
-
-        if (!userId || typeof userId !== "string") {
-            throw new Error("Invalid ID follow.ts")
-        };
-
-        const user = await prisma.user.findUnique({
-            where: {
-                id: userId
-            }
-        });
-
-        if (!user) {
-            throw new Error("Invalid ID follow.ts (2)")
-
-        }
-
-        let updatedFollowingIds = [...(user.followingIds || [])];
-
-        if (req.method == "POST") {
-            updatedFollowingIds.push(userId);
-        }
-
-        if (req.method == "DELETE") {
-            updatedFollowingIds = updatedFollowingIds.filter(followingId => followingId !== userId);
-        }
-
-        const updatedUser = await prisma.user.update({
-            where: { id: currentUser.id },
-            data: { followingIds: updatedFollowingIds }
-        })
-        return res.status(200).json(updatedUser)
+      await prisma.user.update({
+        where: {
+          id: userId,
+        },
+        data: {
+          hasNotification: true,
+        },
+      });
+    } catch (e) {
+      console.log("ERROR N0F");
+      console.log(e);
     }
-    catch (e) {
-        console.log(e)
-        return res.status(400).end()
+
+    if (req.method == "DELETE") {
+      updatedFollowingIds = updatedFollowingIds.filter(
+        (followingId) => followingId !== userId
+      );
     }
+
+    const updatedUser = await prisma.user.update({
+      where: { id: currentUser.id },
+      data: { followingIds: updatedFollowingIds },
+    });
+    return res.status(200).json(updatedUser);
+  } catch (e) {
+    console.log(e);
+    return res.status(400).end();
+  }
 }
